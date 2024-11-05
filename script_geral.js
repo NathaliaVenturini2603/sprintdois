5c5c2f3afe499872ec7a7c864bc4024bbd7f676e
NathaliaVenturini2603 <nathalia.v.dias@aluno.senai.br>
2024-11-05 09:24:25 -0300

    a

--- script_geral.js
+++ script_geral.js
@@ -41,7 +41,7 @@ form.addEventListener('submit', (e) => {
         return
     }
 
-    window.location.href = "home.html"
+    window.location.href = "filmes.html"
 });
 
 function validateloginform(email, password) {
